819d6e428f5d7456549ce84142dde3954098e72f
stephen owino <Sowino939@gmail.com>
2024-03-25 11:25:21 +0300

    create a file

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let multiply= 62
+let num2= multiply/num1
+let num1=8
+function multiply(num1,num2) {num1*num2
+    
+ 
+    
+}
 
-// Write your code here
+ const random=Math.floor(Math.random() * 0) + 1;
+
+ let num3=20
+ let num4=8
+let mod =(num3 % num4);
+ 
+
+
+let max=20
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
